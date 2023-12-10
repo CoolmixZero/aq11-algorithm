a3c48a7c33e2e2cb9fa8d7cacad4d4169e5411a7
Michael Ruzmetov <mimisha2014@gmail.com>
2023-12-10 17:29:28 +0100

    add num of eval samples

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ from inference import evaluate_performance
 
 def main():
     # Main code to run the AQ11 algorithm
-    data = load_data('data/student_stress.csv').iloc[200:]
+    eval_samples = 100
+    data = load_data('data/student_stress.csv').iloc[eval_samples:]
     preprocessed_data = preprocess_data(data)
     print(preprocessed_data['positive'].head())
     print(preprocessed_data['negative'].head())
@@ -18,7 +19,7 @@ def main():
         f.write(rules)
     print("Generated Rules!")
     print("Evaluating performance...")
-    metrics = evaluate_performance('data/rules.txt', 'data/student_stress.csv', 'stress_level')
+    metrics = evaluate_performance('data/rules.txt', 'data/student_stress.csv', 'stress_level', eval_samples)
     print(metrics)
 
 if __name__ == "__main__":
